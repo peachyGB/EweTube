0199400a351fec49be7f8c752711974d478c5c68
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-31 10:26:16 -0400

    fixed rood error

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
 // Parent Component
 
 import "./App.css";
-import React from "react"
-import Header from "./Header"
-import Search from "./Search"
-import NavBar from "./NavBar"
-import {Route, Switch} from "react-router-dom"
+import React from "react";
+import Header from "./Header";
+import Search from "./Search";
+import NavBar from "./NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" >eweTube</header>
+      <header className="App-header">eweTube</header>
       <NavBar />
       <Switch>
-        <Route path="/Header" >
+        <Route path="/Header">
           <Header />
-        </Route>code 
-        <Route path="Search" >
+        </Route>
+        code
+        <Route path="Search">
           <Search />
         </Route>
-        <Route path="NavBar" >
+        <Route path="NavBar">
           <NavBar />
         </Route>
       </Switch>
-
     </div>
   );
 }
