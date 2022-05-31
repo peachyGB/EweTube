e48326d81c7d5724954c23cdd626ed7fed34d81c
Mark <markwilkins86@gmail.com>
2022-05-31 09:42:15 -0400

    fixed i think

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
+// Parent Component
+
 import "./App.css";
 import React from "react"
 import Header from "./Header"
 import Search from "./Search"
 import NavBar from "./NavBar"
-import {Route, Switch} from "react-route-dom"
+import {Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">eweTube</header>
+      <header className="App-header" >eweTube</header>
       <NavBar />
       <Switch>
         <Route path="/Header" >
           <Header />
-        </Route>
+        </Route>code 
         <Route path="Search" >
           <Search />
         </Route>
